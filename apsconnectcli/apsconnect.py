7068291242531ba57cb7854b5a8d89106d4bd0b8
Max Belov <mbelov@odin.com>
2017-12-12 17:25:49 +0300

    Improve error handling for hub config issues

--- apsconnectcli/apsconnect.py
+++ apsconnectcli/apsconnect.py
@@ -667,11 +667,25 @@ def _download_file(url, target=None):
 
 
 def _get_cfg():
-    cfg = json.load(open(CFG_FILE_PATH))
-    if not cfg:
-        print("Run init command.")
+    try:
+        with open(CFG_FILE_PATH) as f:
+            cfg = json.load(f)
+    except IOError as e:
+        if e.errno == 2:
+            print("Could not find connected hub data. "
+                  "Please run the init-hub command to connect Odin Automation hub.")
+        else:
+            print("Could not open configuration file:\n{}".format(e))
         sys.exit(1)
-    return cfg
+    except ValueError:
+        print("Could not parse the configuration file, please re-run "
+              "the init-hub command to regenerate the configuration.")
+        sys.exit(1)
+    except Exception as e:
+        print("Failed to read connected hub configuration. Error message:\n{}".format(e))
+        sys.exit(1)
+    else:
+        return cfg
 
 
 def _to_bytes(raw_str):
