d87de953276fa866049a2134b0c315175ad0f974
Max Belov <mbelov@odin.com>
2018-01-11 14:53:58 +0300

    Add tests

--- tests/test_apsconnect_internals.py
+++ tests/test_apsconnect_internals.py
@@ -5,6 +5,7 @@ from pkg_resources import DistributionNotFound
 from unittest import TestCase
 
 from apsconnectcli.apsconnect import (
+    GITHUB_RELEASES_PAGE,
     KUBE_FILE_PATH,
     _assert_hub_version,
     _osaapi_raise_for_status,
@@ -20,6 +21,9 @@ from apsconnectcli.apsconnect import (
     _to_bytes,
     bin_version,
     get_version,
+    get_latest_version,
+    main,
+    APSConnectUtil,
 )
 
 from apsconnectcli.cluster import AvailabilityCheckResult
@@ -753,6 +757,44 @@ class AssertHubVersion(TestCase):
             sys_mock.exit.assert_called_with(1)
 
 
+class TestVersion(TestCase):
+    def test_latest_version(self):
+        with patch('apsconnectcli.apsconnect.get_version') as version_mock, \
+            patch('apsconnectcli.apsconnect.get_latest_version') as latest_version_mock, \
+                patch(_BUILTINS_PRINT) as print_mock:
+
+            version_mock.return_value = '1.2.3'
+            latest_version_mock.return_value = '1.2.3'
+            APSConnectUtil().version()
+
+        self.assertEqual(print_mock.call_count, 1)
+        self.assertTrue('1.2.3' in print_mock.call_args[0][0])
+
+    def test_outdated_version(self):
+        with patch('apsconnectcli.apsconnect.get_version') as version_mock, \
+            patch('apsconnectcli.apsconnect.get_latest_version') as latest_version_mock, \
+                patch(_BUILTINS_PRINT) as print_mock:
+
+            version_mock.return_value = '1.2.3'
+            latest_version_mock.return_value = '1.2.4'
+            APSConnectUtil().version()
+
+        self.assertEqual(print_mock.call_count, 2)
+        self.assertTrue('1.2.4' in print_mock.call_args[0][0])
+
+    def test_unknown_version(self):
+        with patch('apsconnectcli.apsconnect.get_version') as version_mock, \
+            patch('apsconnectcli.apsconnect.get_latest_version'), \
+                patch(_BUILTINS_PRINT) as print_mock:
+
+            version_mock.return_value = None
+
+            APSConnectUtil().version()
+
+        self.assertEqual(print_mock.call_count, 1)
+        self.assertTrue(GITHUB_RELEASES_PAGE in print_mock.call_args[0][0])
+
+
 class TestHelpers(TestCase):
     def test_bin_version_ok(self):
         with patch('apsconnectcli.apsconnect.sys') as sys_mock, \
@@ -783,3 +825,37 @@ class TestHelpers(TestCase):
             result = get_version()
 
         self.assertEqual(result, 'v100500')
+
+    def test_get_latest_version_ok(self):
+        with patch('apsconnectcli.apsconnect.get') as get_mock:
+            get_mock.return_value.json.return_value = {'tag_name': 'v123'}
+            result = get_latest_version()
+
+        self.assertEqual(result, '123')
+
+    def test_get_latest_version_error(self):
+        with patch('apsconnectcli.apsconnect.get') as get_mock:
+            get_mock.return_value = 'Definitely not JSON'
+            result = get_latest_version()
+
+        self.assertIsNone(result)
+
+    def test_main_prints_version(self):
+        with patch('apsconnectcli.apsconnect.fire'), \
+             patch('apsconnectcli.apsconnect.get_version') as get_version_mock, \
+                patch(_BUILTINS_PRINT) as print_mock:
+
+            get_version_mock.return_value = '100.500'
+            main()
+
+        self.assertTrue('100.500' in print_mock.call_args[0][0])
+
+    def test_main_prints_error_and_exists_if_there_are_problems(self):
+        with patch('apsconnectcli.apsconnect.fire') as fire_mock, \
+             patch('apsconnectcli.apsconnect.get_version') as get_version_mock, \
+                patch(_BUILTINS_PRINT) as print_mock:
+
+            get_version_mock.return_value = '100.500'
+            main()
+
+        self.assertTrue('100.500' in print_mock.call_args[0][0])
