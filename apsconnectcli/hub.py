97c3bbc0d83f0ba8b773d70a0c0ee639b5b55cad
Marc Serrat <Marc.SerratBote@ingrammicro.com>
2020-04-05 23:11:46 +0200

    Updates for version 19 of Connect

--- apsconnectcli/hub.py
+++ apsconnectcli/hub.py
@@ -24,16 +24,31 @@ def osaapi_raise_for_status(r):
             raise Exception("Error: Unknown {}".format(r))
 
 
+def apsapi_raise_for_status(r):
+    try:
+        r.raise_for_status()
+    except Exception as e:
+        if 'error' in r.json():
+            err = "{} {}".format(r.json()['error'], r.json()['message'])
+        else:
+            err = str(e)
+        print("Hub APS API response {} code.\n"
+              "Error: {}".format(r.status_code, err))
+        sys.exit(1)
+
+
 class Hub(object):
     osaapi = None
     aps = None
     hub_id = None
+    extension_id = None
 
     def __init__(self):
         config = get_config()
         self.osaapi = osaapi.OSA(**{k: config[k] for k in RPC_CONNECT_PARAMS})
         self.aps = APS(self.get_admin_token())
         self.hub_id = self._get_id()
+        self.extension_id = self._get_extension_id()
 
     @staticmethod
     def configure(hub_host, user='admin', pwd='1q2w3e', use_tls=False, port=8440, aps_host=None,
@@ -71,17 +86,25 @@ class Hub(object):
         return tree.find('ClientVersion').text
 
     @staticmethod
-    def _assert_supported_version(version):
+    def _assert_supported_version(version, experimental=False):
         supported = False
 
-        match = re.match(r'^oa-(?P<major>\d)\.(?P<minor>\d+)-', version)
+        match = re.match(r'^(oa|cb)-(?P<major>\d)\.(?P<minor>\d+)-', version)
         if match:
             major = int(match.groupdict()['major'])
             minor = int(match.groupdict()['minor'])
-            supported = (major == 7 and minor > 0) or major > 7
+            if experimental:
+                supported = (major >= 8 and minor >= 3) or major > 8
+                if not supported:
+                    print(
+                        "Experimental functionality requires Hub version 8.3 " +
+                        "and above, got {}".format(version))
+                    sys.exit(1)
+            else:
+                supported = (major == 8 and minor > 0) or major > 8
 
         if not supported:
-            print("Hub 7.1 version or above needed, got {}".format(version))
+            print("Hub 8.0 version or above needed, got {}".format(version))
             sys.exit(1)
 
     @staticmethod
@@ -104,6 +127,17 @@ class Hub(object):
 
         return resclass_name or 'rc.saas.resource.unit'
 
+    def _get_extension_id(self):
+        url = 'aps/2/resources?implementing(http://odin.com/servicesSelector/globals/2.0)'
+        r = self.aps.get(url)
+        try:
+            data = json.loads(r.content.decode('utf-8'))
+        except ValueError:
+            print("APSController provided non-json format")
+            sys.exit(1)
+        else:
+            return data[0]['aps']['id'] if data else None
+
     def _get_id(self):
         url = 'aps/2/resources?implementing(http://parallels.com/aps/types/pa/poa/1.0)'
         r = self.aps.get(url)
@@ -125,6 +159,33 @@ class Hub(object):
                                           bool_value=not bool(disable))
         osaapi_raise_for_status(r)
         print("APS Development mode {}.".format('DISABLED' if disable else 'ENABLED'))
+
+    def check_package_operation(self, package, experimental):
+        url = '/aps/2/types?implementing({}/app)'.format(package.connector_id)
+        r = self.aps.get(url)
+        try:
+            data = json.loads(r.content.decode('utf-8'))
+        except ValueError:
+            print("APSController provided non-json format")
+            sys.exit(1)
+        if len(data) == 0:
+            print("INFO: package is not installed")
+            return "install"
+        latest = 0
+        for type in data:
+            match = re.match(r'{}/app/(?P<major>\d+)\.0'.format(package.connector_id), type['id'])
+            if match:
+                major = int(match.groupdict()['major'])
+                if int(latest) < major:
+                    latest = major
+        if int(latest) == int(package.version):
+            return "createRTs"
+        elif int(latest) > int(package.version):
+            print("Is not possible to import a version older than existing one at hub")
+            sys.exit(1)
+        print("Upgrade operation from version {} to version {} required".format(latest,
+                                                                                package.version))
+        return "upgrade"
 
     def import_package(self, package):
         args = {'package_url': package.source} if package.is_http else {
@@ -232,17 +293,173 @@ class Hub(object):
 
         return rt_ids
 
-    def _create_counter_rts(self, package, app_id):
-        rt_ids = {}
-        for counter, schema in package.counters.items():
-            oa_unit_type = Hub._get_resclass_name(schema['unit'])
+    def _get_package(self, instance_uuid):
+        r = self.aps.get('aps/2/resources/{}'.format(instance_uuid))
+        apsapi_raise_for_status(r)
+        return r.json()['aps']['package']['id']
+
+    def _is_service_profile_supported(self, instance_uuid):
+        package_uuid = self._get_package(instance_uuid)
+        r = self.aps.get('aps/2/packages/{}'.format(package_uuid))
+        apsapi_raise_for_status(r)
+        services = r.json()['services']
+        if 'itemProfile' in services.keys():
+            print("INFO: Service Profile is Supported")
+            return True
+        print("INFO: Service profile is not supported")
+        return False
+
+    def _get_item_info_from_local_id(self, product, local_id):
+        payload = {
+            'product': product,
+            'local_id': local_id,
+        }
+        r = self.aps.post('aps/2/resources/{}/itemInfo'.format(self.extension_id), json=payload)
+        try:
+            apsapi_raise_for_status(r)
+            data = json.loads(r.content.decode('utf-8'))
+        except ValueError:
+            print("Error while decoding item information")
+            sys.exit(1)
+        if data.get('local_id'):
+            return data
+        print("ERROR: Connector contains non valid items, please contact CloudBlue Support")
+        sys.exit(1)
+
+    def _create_service_profile(self, package, counter, title):
+        item_info = self._get_item_info_from_local_id(package.product_id, counter)
+        payload = {
+            'aps': {'type': '{}/{}/{}.{}'.format(
+                package.connector_id, "itemProfile", package.version, package.release)},
+            'profileName': title,
+            'mpn': item_info.get('mpn'),
+            'itemId': item_info.get('id'),
+        }
+
+        r = self.aps.post('aps/2/resources/', json=payload)
+        try:
+            r.raise_for_status()
+        except Exception as e:
+            if 'error' in r.json():
+                err = "{} {}".format(r.json()['error'], r.json()['message'])
+            else:
+                err = str(e)
+            print("Adding service profile {} FAILED.\n"
+                  "Hub APS API response {} code.\n"
+                  "Error: {}".format(counter, r.status_code, err))
+            sys.exit(1)
+
+        return r.json()['aps']['id']
+
+    def _create_reseller_profile(self, package):
+        payload = {
+            'aps': {'type': '{}/{}/{}.{}'.format(
+                package.connector_id, "marketPlaceProfile", package.version, package.release)},
+            'vendorContract': '',
+        }
+
+        r = self.aps.post('aps/2/resources/', json=payload)
+        apsapi_raise_for_status(r)
+        return r.json()['aps']['id']
+
+    def _type_manager_available(self):
+        r = self.aps.get('aps/2/services/resource-type-manager')
+        try:
+            apsapi_raise_for_status(r)
+        except Exception:
+            print("ERROR: Operations with type manager app needed, but not available on this\n" +
+                  "HUB.\nCreation of new resource types is not possible without it.\n" +
+                  "Please contact CloudBlue support to update your Hub or use Configure Option " +
+                  "available on the connector instance on Hub control panel")
+            sys.exit(1)
+        return r.json()
+
+    def _exists_marketplace_profile_resource(self, package):
+        r = self.aps.get('aps/2/resources?implementing({}/{}/{}.{})'.format(
+            package.connector_id, "marketPlaceProfile", package.version, package.release))
+        try:
+            data = json.loads(r.content.decode('utf-8'))
+            if data[0]['aps']['id']:
+                return True
+            return False
+        except Exception:
+            return False
+
+    def _get_existing_ref_rts(self, app_id):
+        self._type_manager_available()
+        payload = {
+            'resclass_name': 'rc.saas.countedlenk',
+        }
+        rts = self.osaapi.getResourceTypesByClass(**payload)
+        existing_resources = []
+        for resource in rts['result']:
+            aps_rt = json.loads(
+                self.aps.get('aps/2/services/resource-type-manager/resourceTypes/{}'.format(
+                    resource['resource_type_id'])).content.decode('utf-8'))
+            if 'app_id' in aps_rt['activationParameters'] and int(aps_rt['activationParameters'][
+                                                                      'app_id']) == int(app_id):
+                existing_resources.append(aps_rt['activationParameters']['resource_id'])
+        return existing_resources
+
+    def _find_existing(self, resource, app_id):
+        aps_rt = json.loads(
+            self.aps.get('aps/2/services/resource-type-manager/resourceTypes/{}'.format(
+                resource['resource_type_id'])).content.decode('utf-8'))
+        if 'app_id' in aps_rt['activationParameters'] and int(
+                aps_rt['activationParameters'][
+                    'app_id']) == int(app_id):
+            return aps_rt['activationParameters']['resource_id']
+        return False
+
+    def _get_existing_counter_rts(self, app_id):
+        self._type_manager_available()
+        existing_resources = []
+        rt_classes = ['rc.saas.resource.kbps',
+                      'rc.saas.resource',
+                      'rc.saas.resource.mbh',
+                      'rc.saas.resource.mhz',
+                      'rc.saas.resource.mhzh',
+                      'rc.saas.resource.unit',
+                      'rc.saas.resource.unith',
+                      ]
+        for rt_class in rt_classes:
             payload = {
-                'resclass_name': oa_unit_type,
-                'name': '{}'.format(schema.get('title')),
+                'resclass_name': rt_class,
+            }
+            rts = self.osaapi.getResourceTypesByClass(**payload)
+            for resource in rts['result']:
+                existing = self._find_existing(resource=resource, app_id=app_id)
+                if existing:
+                    existing_resources.append(existing)
+        return existing_resources
+
+    def _create_counted_ref_rts(self, package, app_id, update=False):
+        rt_ids = {}
+        if update:
+            exists_marketplace_profile = self._exists_marketplace_profile_resource(package)
+            if not exists_marketplace_profile:
+                print("ERROR: Create new resource types for this package version requested \n" +
+                      "using new experimental mode, but seams this package uses counter mode.\n" +
+                      "update operation aborted, either use normal mode, either ensure " +
+                      "marketProfile resources exists")
+                sys.exit(1)
+            existing_resources = self._get_existing_ref_rts(app_id)
+        for counter, schema in package.counters.items():
+            if update and counter in existing_resources:
+                continue
+            title = schema.get('title')
+            resource_uid = self._create_service_profile(package, counter, title)
+            payload = {
+                'resclass_name': 'rc.saas.countedlenk',
+                'name': title,
                 'act_params': [
                     {
                         'var_name': 'app_id',
-                        'var_value': app_id
+                        'var_value': str(app_id)
+                    },
+                    {
+                        'var_name': 'resource_uid',
+                        'var_value': str(resource_uid)
                     },
                     {
                         'var_name': 'service_id',
@@ -250,7 +467,71 @@ class Hub(object):
                     },
                     {
                         'var_name': 'resource_id',
-                        'var_value': counter
+                        'var_value': str(counter)
+                    }
+                ]
+            }
+
+            response = self.osaapi.addResourceType(**payload)
+            osaapi_raise_for_status(response)
+            rt_ids[response['result']['resource_type_id']] = 0
+
+        return rt_ids
+
+    def _create_reseller_link_rt(self, package, app_id):
+        rt_ids = {}
+        resource_uid = self._create_reseller_profile(package)
+        payload = {
+            'resclass_name': 'rc.saas.service.link',
+            'name': 'Vendor Contract ID',
+            'act_params': [
+                {
+                    'var_name': 'app_id',
+                    'var_value': app_id
+                },
+                {
+                    'var_name': 'resource_uid',
+                    'var_value': resource_uid
+                }
+            ]
+        }
+
+        response = self.osaapi.addResourceType(**payload)
+        osaapi_raise_for_status(response)
+        rt_ids[response['result']['resource_type_id']] = 0
+
+        return rt_ids
+
+    def _create_counter_rts(self, package, app_id, update=False):
+        rt_ids = {}
+        if update:
+            exists_marketplace_profile = self._exists_marketplace_profile_resource(package)
+            if exists_marketplace_profile:
+                print("ERROR: Create new resource types for this package version requested \n" +
+                      "but seams this package has been instantiated using experimental mode.\n" +
+                      "update operation aborted, either use experimental mode, either ensure " +
+                      "marketProfile resources does not exists")
+                sys.exit(1)
+            existing_resources = self._get_existing_counter_rts(app_id)
+        for counter, schema in package.counters.items():
+            if update and counter in existing_resources:
+                continue
+            oa_unit_type = Hub._get_resclass_name(schema['unit'])
+            payload = {
+                'resclass_name': oa_unit_type,
+                'name': '{}'.format(schema.get('title')),
+                'act_params': [
+                    {
+                        'var_name': 'app_id',
+                        'var_value': str(app_id)
+                    },
+                    {
+                        'var_name': 'service_id',
+                        'var_value': 'tenant'
+                    },
+                    {
+                        'var_name': 'resource_id',
+                        'var_value': str(counter)
                     },
                 ]
             }
@@ -293,9 +574,16 @@ class Hub(object):
 
         return rt_ids
 
-    def create_rts(self, package, app_id, instance_uuid):
-        rts = self._create_core_rts(package, app_id, instance_uuid)
-        rts.update(self._create_counter_rts(package, app_id))
+    def create_rts(self, package, app_id, instance_uuid, experimental, update_rts=False):
+        rts = {}
+        if not update_rts:
+            rts.update(self._create_core_rts(package, app_id, instance_uuid))
+        if self._is_service_profile_supported(instance_uuid) and experimental:
+            rts.update(self._create_counted_ref_rts(package, app_id, update_rts))
+            if not update_rts:
+                rts.update(self._create_reseller_link_rt(package, app_id))
+        else:
+            rts.update(self._create_counter_rts(package, app_id, update_rts))
         rts.update(self._create_parameter_rts(package, app_id))
         return rts
 
@@ -319,6 +607,88 @@ class Hub(object):
 
         r = self.osaapi.setSTRTLimits(**payload)
         osaapi_raise_for_status(r)
+
+    def check_experimental_support(self):
+        hub_version = self._get_hub_version(self.osaapi)
+        self._assert_supported_version(hub_version, experimental=True)
+
+    def check_connect_hub_app_installed(self):
+        url = 'aps/2/resources?implementing(http://odin.com/servicesSelector/globals/2.0)'
+        r = self.aps.get(url)
+        try:
+            data = json.loads(r.content.decode('utf-8'))
+        except ValueError:
+            print("APSController provided non-json format")
+            sys.exit(1)
+        else:
+            if len(data) == 0:
+                print("ERROR: Connect Hub extension is not installed")
+                print("ERROR: Please follow the instructions available here: " +
+                      "https://connect.cloudblue.com/documentation/extensions/" +
+                      "cloudblue-commerce/reseller-control-panel/")
+                sys.exit(1)
+
+            match = re.match(
+                r'http://odin.com/servicesSelector/globals/(?P<major>\d)\.(?P<minor>\d+)',
+                data[0]['aps']['type'])
+            if match:
+                major = int(match.groupdict()['major'])
+                minor = int(match.groupdict()['minor'])
+                supported = (major >= 2 and minor > 1) or major > 2
+                if not supported:
+                    print("ERROR: Connect Hub extension is outdated")
+                    print("ERROR: Please upgrade it using instructions available here: " +
+                          "https://connect.cloudblue.com/documentation/extensions/" +
+                          "cloudblue-commerce/reseller-control-panel/")
+                    sys.exit(1)
+
+    def get_connections(self, product_id):
+        url = 'aps/2/resources/{}/connectionsInfo'.format(self.extension_id)
+        payload = {
+            "product": product_id
+        }
+        r = self.aps.post(url, json=payload)
+        try:
+            data = json.loads(r.content.decode('utf-8'))
+            if data.get('id'):
+                return data
+            print("ERROR: Product {} has no connection for this HUB.\n".format(product_id) +
+                  "Please access CloudBlue Connect provider panel to create a new one")
+            exit(1)
+
+        except ValueError:
+            print("APSController provided non-json format")
+            sys.exit(1)
+
+    def get_application_id(self, package_id):
+        payload = {
+            'aps_application_id': package_id,
+        }
+
+        r = self.osaapi.aps.getApplications(**payload)
+        osaapi_raise_for_status(r)
+
+        return r['result'][0]['application_id'] or None
+
+    def get_application_instances(self, application_id):
+        payload = {
+            'app_id': application_id,
+        }
+
+        r = self.osaapi.aps.getApplicationInstances(**payload)
+        osaapi_raise_for_status(r)
+
+        return r['result']
+
+    def upgrade_application_instance(self, instance):
+        payload = {
+            'application_instance_id': int(instance),
+        }
+
+        r = self.osaapi.aps.upgradeApplicationInstance(**payload)
+        osaapi_raise_for_status(r)
+
+        return
 
 
 class APS(object):
